9e416665b3ed7fbbac320b951124cb71445495ef
sdim-ua <ingersol@yandex.ua>
2019-01-08 16:26:36 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,14 @@
+
+/* 
+Полнометражная картина
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getMetreFromCentimetre(243));
+    }
+
+    public static int getMetreFromCentimetre(int centimetre) {
+        //напишите тут ваш код
+    }
+}
